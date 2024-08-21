19e2b07a5c0191faf87da8b42600a8e105640690
Paavni-Goyal <paavnigoyal@gmail.com>
2024-08-21 23:07:53 +0530

    Initial Commit

--- api_mongodb/models/Book.model.js
+++ api_mongodb/models/Book.model.js
@@ -1,11 +1,11 @@
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
-// const bookSchema = new mongoose.Schema({
-//   title: { type: String, required: true },
-//   author: { type: String, required: true },
-//   descrip: { type: String, required: true },
-//   price: { type: Number, required: true }
-//   // Remove genre and year if not needed
-// });
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  descrip: { type: String, required: true },
+  price: { type: Number, required: true }
+  // Remove genre and year if not needed
+});
 
-// export const Book = mongoose.model('Book', bookSchema);
+export const Book = mongoose.model('Book', bookSchema);
