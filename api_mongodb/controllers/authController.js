59b99befce3a7c66aadb76d821b1d3d0baacddfa
Paavni-Goyal <paavnigoyal@gmail.com>
2024-09-03 15:30:48 +0530

    Changes

--- api_mongodb/controllers/authController.js
+++ api_mongodb/controllers/authController.js
@@ -3,6 +3,28 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body; 
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).send({ message: 'Invalid credentials' });
+
+    // Compare passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).send({ message: 'Invalid credentials' });
+
+    // Generate token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.send({ token, username: user.username }); // Include username in response
+  } catch (error) {
+    console.error('Login error:', error); // Log error details
+    res.status(500).send({ message: 'Error logging in', error });
+  }
+};
+
+
 // Register
 export const register = async (req, res) => {
   try {
@@ -28,3 +50,6 @@ export const register = async (req, res) => {
     res.status(500).json({ message: 'Error registering user', error: error.message });
   }
 };
+
+
+
